0bb8b10543ad5be78c7a78f06f6f895a1caa34a1
Bertrand Kerautret <bertrand.kerautret@univ-lyon2.fr>
2024-04-05 14:20:57 +0200

    Initial Commit

---
+++ hgffhg/hgffhgApp.swift
@@ -0,0 +1,17 @@
+//
+//  hgffhgApp.swift
+//  hgffhg
+//
+//  Created by Bertrand Kerautret on 05/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct hgffhgApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
